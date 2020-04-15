a029d3914ca03e35e7e1c425e8e1112692bf95dd
Jan Weyrich <janweyrich93@gmail.com>
2020-04-15 11:43:11 +0200

    complete login logic on /dashboard path

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,20 +7,30 @@ from dashboard.spotify_album_models import welcome_from_dict
 
 client_id = spotify_client_properties["client_id"]
 secret_clientid = spotify_client_properties["secret_clientid"]
-callback_uri = "http://localhost:8080/dashboard/albums"
+callback_uri = "http://localhost:8080/dashboard"
 
 def index(request):
-    spotify = SpotifyClient(client_id, secret_clientid, callback_uri)
-    auth_url = spotify.get_spotify_auth_page()
-    context = {
-        'auth_url': auth_url,
-    }
-    return render(request, 'index.html', context)
-
-def show_albums(request):   
     access_code = request.GET.get('code')
     spotify = SpotifyClient(client_id, secret_clientid, callback_uri)
-    spotify.request_access_token(access_code)
+
+    if not access_code:
+        auth_url = spotify.get_spotify_auth_page()
+        context = {
+            'auth_url': auth_url,
+        }
+    else:
+        spotify.request_access_token(access_code)
+        print("-------" + str(spotify.access_token) + "--------_")
+        request.session['access_token'] = spotify.access_token
+        context = {
+            'auth_url': None,
+        }
+    return render(request, 'index.html', context)  
+
+def show_albums(request):
+    access_code = request.session.get('access_token')
+    spotify = SpotifyClient(client_id, secret_clientid, callback_uri, access_code)
+
     albums_response_dict = spotify.get_albums()
 
     result = welcome_from_dict(albums_response_dict)
@@ -28,3 +38,7 @@ def show_albums(request):
         'album_list': result.items,
     }
     return render(request, 'index.html', context)
+
+
+def show_followed_artists(request):
+    pass
