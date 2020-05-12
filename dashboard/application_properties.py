8e9a527651e08ec2d34eee3f6ad8413fac06a6a1
Jan Weyrich <janweyrich93@gmail.com>
2020-05-12 14:55:44 +0200

    fix env variable

--- dashboard/application_properties.py
+++ dashboard/application_properties.py
@@ -3,6 +3,6 @@ import os
 # read env variables or use default value if it does not find one
 spotify_client_properties = {
     "client_id": os.getenv('SPOTIFY_CLIENT_ID', "<INVALID>"),
-    "secret_clientid": os.getenv('SPOTIFY_SECRET_CLIENT_ID', "<INVALID>"),
+    "secret_clientid": os.getenv('SPOTIFY_CLIENT_ID_SECRET', "<INVALID>"),
     "callback_url": os.getenv('SPOTIFY_CALLBACK_URL', "http://localhost:8080/dashboard/albums")
 }
